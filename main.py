290e719ee81954c550c8839681056d10113f4a39
Laf3aan <rafan.atilla1212@gmail.com>
2026-07-16 20:27:15 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+from model import get_class
+import os, random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''The duck command returns the photo of the duck'''
+    print('hello')
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("You forgot to upload the image :(")
+
+@bot.command()
+async def percent(ctx, a: float, b: float):
+    x = a / 100 * b
+    await ctx.send(f"{a} % of {b} equals to {x}")
+
+bot.run('empty')
